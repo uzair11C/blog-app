c47fc271aa78abd4a877933faf31d0b0b59b74e6
Uzair Abdullah <uzairabdullah973@gmail.com>
2022-09-14 15:08:14 +0500

    Create dashboard.js

---
+++ src/components/dashboard.js
@@ -0,0 +1,15 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import Appbar from './Appbar'
+
+const Dashboard = () => 
+{
+    return(
+        <div> 
+        <Appbar />
+            <Button variant='contained'>WTH</Button>
+        </div>
+    )
+}
+
+export default Dashboard
