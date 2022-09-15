09651a49359fa6284cbab41704662c421c270cc2
Uzair Abdullah <uzairabdullah973@gmail.com>
2022-09-15 11:15:23 +0500

    added all posts context

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CurrentUserContext } from './contexts/currentUserContext'
 import { AllUsersContext } from './contexts/allUsersContext';
+import { AllPostsContext } from './contexts/allPostsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +12,9 @@ root.render(
     <Router>
       <AllUsersContext>
         <CurrentUserContext>
-          <App />
+          <AllPostsContext>
+            <App />
+          </AllPostsContext>
         </CurrentUserContext>
       </AllUsersContext>
     </Router>
