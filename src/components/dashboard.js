d0d0825f5ff0a58b9bf9c7388cf5609dd018ac4a
Uzair Abdullah <uzairabdullah973@gmail.com>
2022-09-15 11:16:21 +0500

    split into components

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,17 +1,8 @@
-import { Stack, Container, Divider, Paper, Grid, Typography } from '@mui/material'
+import { Stack, Container, Grid, Button } from '@mui/material'
 import React from 'react'
 import Appbar from './Appbar'
-
-import { styled } from '@mui/material/styles';
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: '#1A2027',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: 'white',
-  height: '80px'
-}));
+import Bloggers from './bloggers'
+import PostsComp from './postsComp'
 
 const Dashboard = () => 
 {
@@ -22,69 +13,35 @@ const Dashboard = () =>
                 maxWidth='lg'
                 sx={{
                     height:'85vh',
-                    display:'flex',
-                    justifyContent:'center',
-                    alignItems:'center'
+                    pt: '30px'
                 }}
                     >
-                <Grid container spacing={3}>
-                    <Grid item xs={12} sm={12} md={8}>
-                        <div
-                            sx={{}}>
-                            <Typography variant='h3' component='h2'
-                                sx={{
-                                    fontWeight: 700
-                                }}
-                            >
-                                All your posts
-                            </Typography>
-                            <Stack
-                                direction='column-reverse'
-                                justifyContent='center'
-                                alignItems='flex-start' 
-                                divider={<Divider flexItem />}
-                                spacing={2}
-                            >
-                                <Paper>post</Paper>
-                                <Paper>post</Paper>
-                                <Paper>post</Paper>
-                            </Stack>
-                        </div>
+                <Grid container spacing={5}>
+                    <Grid item xs={12} sm={12} md={8} sx={{}}>
+                        <PostsComp />
                     </Grid>
-                    <Grid item xs={12} sm={12} md={4}>
-                        <Paper 
-                            elevation={5}
+                    <Grid 
+                        item 
+                        xs={12} 
+                        sm={12} 
+                        md={4}
+                        sx={{
+                            display:'flex',
+                            flexDirection: 'column',
+                            justifyContent:'center',
+                            alignItems:'stretch',
+                        }}
+                    >
+                        <Button
+                            variant='contained'
+                            color='secondary'
                             sx={{
-                                borderRadius: '12px',
-                                p: '20px'
+                                fontSize:'18px',
                             }}
                         >
-                            <Stack
-                                direction='column'
-                                justifyContent='center'
-                                alignItems='flex-start'
-                            >
-                                <Typography  
-                                    variant='h5'
-                                    component='h3'
-                                    sx={{
-                                        fontWeight: 700,
-                                        mb: '5px',
-                                    }}
-                                >
-                                    All our bloggers
-                                </Typography >
-                                <Typography variant='h6' component='div'>
-                                    A User
-                                </Typography >
-                                <Typography variant='h6' component='div'>
-                                    A User
-                                </Typography >
-                                <Typography variant='h6' component='div'>
-                                    A User
-                                </Typography >
-                            </Stack>
-                        </Paper>
+                            Create a new post
+                        </Button>
+                        <Bloggers />
                     </Grid>
                 </Grid>
             </Container>
