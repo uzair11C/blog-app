3ff8eec7b2aff22d8848a625c8d2629f2017829c
Uzair Abdullah <uzairabdullah973@gmail.com>
2022-09-16 16:42:51 +0500

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -24,7 +24,7 @@ const Login = () =>
                     email: Yup.string().email('Invalid Email').required('Required'),
                 }
             ),
-            onSubmit: (values) =>
+            onSubmit: () =>
             {
                 submitHandler()
             }
