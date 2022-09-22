89c7a67969ce4e7b6c3c3904c746ff848754679d
Uzair Abdullah <uzairabdullah973@gmail.com>
2022-09-22 10:42:42 +0500

    formatting

--- src/App.js
+++ src/App.js
@@ -1,57 +1,49 @@
 import React, { useContext, useEffect } from "react";
-import { CurrentUser } from './contexts/currentUserContext'
-import {
-  Routes,
-  Route,
-  useNavigate 
-} from "react-router-dom";
+import { CurrentUser } from "./contexts/currentUserContext";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Dashboard from "./components/dashboard";
-import Login from "./components/login"
-import MyPostsPage from './components/myPostsPage'
-import BlogPage from './components/blogPage'
+import Login from "./components/login";
+import MyPostsPage from "./components/myPostsPage";
+import BlogPage from "./components/blogPage";
 import CreatePost from "./components/createPost";
 import EditPost from "./components/editPost";
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from 'react-toastify'
+import { ToastContainer } from "react-toastify";
 
-function App() 
-{
-  const navigate = useNavigate()
-  const [currentUser, setCurrentUser] = useContext(CurrentUser)
+function App() {
+	const navigate = useNavigate();
+	const [currentUser, setCurrentUser] = useContext(CurrentUser);
 
-  useEffect(
-    () => {
-      currentUser.id === null
-      ?
-      navigate('/login',{replace:'true'})
-      :
-      navigate('/dashboard',{replace:'true'})
-    },[currentUser.id])
+	useEffect(() => {
+		currentUser.id === null
+			? navigate("/login", { replace: "true" })
+			: navigate("/dashboard", { replace: "true" });
+	}, [currentUser.id]);
 
-  return (
-    <div>
-      <Routes>
-        <Route exact path='/login' element={<Login />} /> 
-        <Route exact path='/dashboard' element={<Dashboard />} />
-        <Route exact path='/user/posts' element={<MyPostsPage />} />
-        <Route exact path='/post/:id' element={<BlogPage />} />
-        <Route exact path='/user/posts/:id' element={<BlogPage />} />
-        <Route exact path='/create-post' element={<CreatePost />} />
-        <Route exact path='/edit-post/:id' element={<EditPost />} />
-      </Routes>
-      <ToastContainer 
-        position="top-right"
-        autoClose={3500}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </div>
-  );
+	return (
+		<div className="app">
+			<Routes>
+				<Route exact path="/login" element={<Login />} />
+				<Route exact path="/dashboard" element={<Dashboard />} />
+				<Route exact path="/user/posts" element={<MyPostsPage />} />
+				<Route exact path="/post/:id" element={<BlogPage />} />
+				<Route exact path="/user/posts/:id" element={<BlogPage />} />
+				<Route exact path="/create-post" element={<CreatePost />} />
+				<Route exact path="/edit-post/:id" element={<EditPost />} />
+			</Routes>
+			<ToastContainer
+				position="top-right"
+				autoClose={3500}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
+		</div>
+	);
 }
 
 export default App;
