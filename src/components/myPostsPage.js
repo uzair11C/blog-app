6a1155007fdd9623fd7446b947df99604333e45e
Uzair Abdullah <uzairabdullah973@gmail.com>
2022-09-22 12:50:11 +0500

    Update myPostsPage.js

--- src/components/myPostsPage.js
+++ src/components/myPostsPage.js
@@ -1,57 +1,58 @@
-import { Container, Grid, Button } from '@mui/material'
-import Appbar from './Appbar'
-import MyPosts from './myPosts'
-import Bloggers from './bloggers'
-import { Link } from 'react-router-dom'
+import { Container, Grid, Button } from "@mui/material";
+import Appbar from "./Appbar";
+import MyPosts from "./myPosts";
+import Bloggers from "./bloggers";
+import { Link } from "react-router-dom";
 
-function MyPostsPage()
-{
-    return(
-        <>
-            <Appbar />
-            <Container
-                maxWidth='lg'
-                sx={{
-                    height:'85vh',
-                    pt: '30px'
-                }}
-            >
-                <Grid container spacing={5}>
-                    <Grid item xs={12} sm={12} md={8} sx={{}}>
-                        <MyPosts />
-                    </Grid>
-                    <Grid 
-                        item 
-                        xs={12} 
-                        sm={12} 
-                        md={4}
-                        sx={{
-                            display:'flex',
-                            flexDirection: 'column',
-                            justifyContent:'center',
-                            alignItems:'stretch',
-                        }}
-                    >
-                        <Button
-                            variant='contained'
-                            color='secondary'
-                            sx={{
-                                fontSize:'18px',
-                            }}
-                        >
-                            <Link 
-                                style={{textDecoration:'none',color:'inherit'}}
-                                to='/create-post'
-                            >
-                                Create a new post
-                            </Link>
-                        </Button>
-                        <Bloggers />
-                    </Grid>
-                </Grid>
-            </Container>
-        </>
-    )
+function MyPostsPage() {
+	return (
+		<>
+			<Appbar />
+			<Container
+				maxWidth="lg"
+				sx={{
+					height: "85vh",
+					pt: "30px",
+				}}
+			>
+				<Grid container spacing={5}>
+					<Grid item xs={12} sm={12} md={8} sx={{}}>
+						<MyPosts />
+					</Grid>
+					<Grid
+						item
+						xs={12}
+						sm={12}
+						md={4}
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+							alignItems: "stretch",
+						}}
+					>
+						<Button
+							variant="contained"
+							color="secondary"
+							sx={{
+								fontSize: "18px",
+								textTransform: "none",
+								background: "#1597BB",
+							}}
+						>
+							<Link
+								style={{ textDecoration: "none", color: "inherit" }}
+								to="/create-post"
+							>
+								Create a new post
+							</Link>
+						</Button>
+						<Bloggers />
+					</Grid>
+				</Grid>
+			</Container>
+		</>
+	);
 }
 
-export default MyPostsPage
+export default MyPostsPage;
