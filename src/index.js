d8f8b61411b24621930bbacbe25ddc8bb215b3b1
Uzair Abdullah <uzairabdullah973@gmail.com>
2022-09-22 10:44:24 +0500

    added stylesheet

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'
-import { BrowserRouter as Router } from 'react-router-dom';
-import { CurrentUserContext } from './contexts/currentUserContext'
-import { AllUsersContext } from './contexts/allUsersContext';
-import { AllPostsContext } from './contexts/allPostsContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CurrentUserContext } from "./contexts/currentUserContext";
+import { AllUsersContext } from "./contexts/allUsersContext";
+import { AllPostsContext } from "./contexts/allPostsContext";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <AllUsersContext>
-      <CurrentUserContext>
-        <AllPostsContext>
-          <App />
-        </AllPostsContext>
-      </CurrentUserContext>
-    </AllUsersContext>
-  </Router>
-)
+	<Router>
+		<AllUsersContext>
+			<CurrentUserContext>
+				<AllPostsContext>
+					<App />
+				</AllPostsContext>
+			</CurrentUserContext>
+		</AllUsersContext>
+	</Router>
+);
