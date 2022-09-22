274c49f3752c5d8556cf5580bbb1eae8979c7def
Uzair Abdullah <uzairabdullah973@gmail.com>
2022-09-22 12:49:20 +0500

    create post text

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,58 +1,60 @@
-import { Container, Grid, Button } from '@mui/material'
-import React from 'react'
-import Appbar from './Appbar'
-import Bloggers from './bloggers'
-import PostsComp from './postsComp'
-import { Link } from 'react-router-dom'
+import { Container, Grid, Button } from "@mui/material";
+import React from "react";
+import Appbar from "./Appbar";
+import Bloggers from "./bloggers";
+import PostsComp from "./postsComp";
+import { Link } from "react-router-dom";
+import CapitalizeFirstLetter from "./capitalize";
 
-const Dashboard = () => 
-{
-    return(
-        <> 
-            <Appbar />
-            <Container 
-                maxWidth='lg'
-                sx={{
-                    height:'85vh',
-                    pt: '30px'
-                }}
-                    >
-                <Grid container spacing={5}>
-                    <Grid item xs={12} sm={12} md={8} sx={{}}>
-                        <PostsComp />
-                    </Grid>
-                    <Grid 
-                        item 
-                        xs={12} 
-                        sm={12} 
-                        md={4}
-                        sx={{
-                            display:'flex',
-                            flexDirection: 'column',
-                            justifyContent:'center',
-                            alignItems:'stretch',
-                        }}
-                    >
-                        <Button
-                            variant='contained'
-                            color='secondary'
-                            sx={{
-                                fontSize:'18px',
-                            }}
-                        >
-                            <Link 
-                                style={{textDecoration:'none',color:'inherit'}}
-                                to='/create-post'
-                            >
-                                Create a new post
-                            </Link>
-                        </Button>
-                        <Bloggers />
-                    </Grid>
-                </Grid>
-            </Container>
-        </>
-    )
-}
+const Dashboard = () => {
+	return (
+		<>
+			<Appbar />
+			<Container
+				maxWidth="lg"
+				sx={{
+					height: "85vh",
+					pt: "30px",
+				}}
+			>
+				<Grid container spacing={5}>
+					<Grid item xs={12} sm={12} md={8} sx={{}}>
+						<PostsComp />
+					</Grid>
+					<Grid
+						item
+						xs={12}
+						sm={12}
+						md={4}
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+							alignItems: "stretch",
+						}}
+					>
+						<Button
+							variant="contained"
+							color="secondary"
+							sx={{
+								fontSize: "18px",
+								textTransform: "none",
+								background: "#1597BB",
+							}}
+						>
+							<Link
+								style={{ textDecoration: "none", color: "inherit" }}
+								to="/create-post"
+							>
+								{CapitalizeFirstLetter("Create a new post")}
+							</Link>
+						</Button>
+						<Bloggers />
+					</Grid>
+				</Grid>
+			</Container>
+		</>
+	);
+};
 
-export default Dashboard
+export default Dashboard;
