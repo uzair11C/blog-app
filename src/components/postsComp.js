b922589ca5a3662b13676f77ca341d02bec2ef8c
Uzair Abdullah <uzairabdullah973@gmail.com>
2022-09-22 16:21:29 +0500

    theme

--- src/components/postsComp.js
+++ src/components/postsComp.js
@@ -1,5 +1,12 @@
 import React, { useContext } from "react";
-import { Stack, Divider, Typography, Card, Box } from "@mui/material";
+import {
+	Stack,
+	Divider,
+	Typography,
+	Card,
+	Box,
+	Pagination,
+} from "@mui/material";
 import { AllPosts } from "../contexts/allPostsContext";
 import { Link } from "react-router-dom";
 import CapitalizeFirstLetter from "./capitalize";
@@ -8,75 +15,89 @@ const PostsComp = () => {
 	const posts = useContext(AllPosts);
 
 	return (
-		<Card
-			raised
-			sx={{
-				mt: "30px",
-				p: "30px",
-				pb: "40px",
-				maxHeight: "60vh",
-				backgroundColor: "#9A0F98",
-				color: "#fff",
-			}}
-		>
-			<Typography
-				variant="h4"
-				component="h2"
+		<>
+			<Card
+				raised
 				sx={{
-					fontWeight: 700,
+					mt: "30px",
+					p: "30px",
+					pb: "40px",
+					maxHeight: "63vh",
+					backgroundColor: "#9A0F98",
+					color: "#fff",
 				}}
 			>
-				Latest Posts for You:
-			</Typography>
-			<Box
-				sx={{
-					maxHeight: "55vh",
-					overflow: "auto",
-					"&::-webkit-scrollbar": {
-						width: "12px",
-					},
-					"&::-webkit-scrollbar-thumb": {
-						background: "#160040",
-						borderRadius: "5px",
-					},
-				}}
-			>
-				<Stack
-					direction="column-reverse"
-					justifyContent="center"
-					alignItems="flex-start"
-					divider={<Divider flexItem />}
-					spacing={2}
-					sx={{ mt: "20px" }}
+				<Typography
+					variant="h4"
+					component="h2"
+					sx={{
+						fontWeight: 700,
+					}}
 				>
-					{posts &&
-						posts.map((post) => (
-							<Typography
-								key={post.id}
-								variant="h6"
-								component="div"
-								sx={{
-									letterSpacing: "0.1rem",
-									textDecoration: "none",
-									color: "#fff",
-									opacity: "0.7",
-									"&:hover": {
-										color: "#160040",
-										transition: "0.3s ease-in-out",
-									},
-								}}
-							>
-								<Link
-									style={{ color: "inherit", textDecoration: "none" }}
-									to={`/post/${post.id}`}
+					Latest Posts for You:
+				</Typography>
+				<Box
+					sx={{
+						maxHeight: "55vh",
+						overflow: "auto",
+						"&::-webkit-scrollbar": {
+							width: "12px",
+						},
+						"&::-webkit-scrollbar-thumb": {
+							background: "#160040",
+							borderRadius: "5px",
+						},
+					}}
+				>
+					<Stack
+						direction="column-reverse"
+						justifyContent="center"
+						alignItems="flex-start"
+						divider={<Divider flexItem />}
+						spacing={2}
+						sx={{ mt: "20px" }}
+					>
+						{posts &&
+							posts.map((post) => (
+								<Typography
+									key={post.id}
+									variant="h6"
+									component="div"
+									sx={{
+										letterSpacing: "0.1rem",
+										textDecoration: "none",
+										color: "#fff",
+										opacity: "0.7",
+										"&:hover": {
+											color: "#160040",
+											transition: "0.3s ease-in-out",
+										},
+									}}
 								>
-									{CapitalizeFirstLetter(post.title)}
-								</Link>
-							</Typography>
-						))}
-				</Stack>
-			</Box>
-		</Card>
+									<Link
+										style={{ color: "inherit", textDecoration: "none" }}
+										to={`/post/${post.id}`}
+									>
+										{CapitalizeFirstLetter(post.title)}
+									</Link>
+								</Typography>
+							))}
+					</Stack>
+				</Box>
+				{/* <Stack justifyContent={"center"} alignItems={"center"}>
+					<Pagination
+						count={2}
+						//variant="outlined"
+						size="large"
+						color="primary"
+						sx={{
+							// background: "#2E0249",
+							mb: "50px",
+						}}
+					/>
+				</Stack> */}
+			</Card>
+		</>
 	);
 };
 
